947553b4cfff01597f30e86734723d918513038b
Victoria Shumskaya <shumvikashum@gmail.com>
2025-07-28 20:09:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+В этой папке будут храниться все необходимые SVG-файлы для сайта.
+[github]:https://github.com/vikusias/zakrivayuschiy-teg-f
--- scripts/like.js
+++ scripts/like.js
@@ -8,9 +8,9 @@
 Если эти классы поменять в HTML, скрипт перестанет работать. Будьте аккуратны.
 */
 
-const likeHeartArray = document.querySelectorAll('.like-icon');
-const likeButtonArray = document.querySelectorAll('.card__like-button');
-const iconButtonArray = document.querySelectorAll('.card__icon-button');
+const likeHeartArray = document.querySelectorAll(".like-icon");
+const likeButtonArray = document.querySelectorAll(".card__like-button");
+const iconButtonArray = document.querySelectorAll(".card__icon-button");
 
 iconButtonArray.forEach((iconButton, index) => {
   iconButton.onclick = () =>
@@ -22,19 +22,19 @@ likeButtonArray.forEach((button, index) => {
 });
 
 function toggleIsLiked(heart, button) {
-  heart.classList.toggle('is-liked');
+  heart.classList.toggle("is-liked");
   setButtonText(heart, button);
 }
 
 function setButtonText(heart, button) {
-  if ([...heart.classList].includes('is-liked')) {
+  if ([...heart.classList].includes("is-liked")) {
     setTimeout(
-      () => (button.querySelector('.button__text').textContent = 'Unlike'),
+      () => (button.querySelector(".button__text").textContent = "Unlike"),
       500
     );
   } else {
     setTimeout(
-      () => (button.querySelector('.button__text').textContent = 'Like'),
+      () => (button.querySelector(".button__text").textContent = "Like"),
       500
     );
   }
